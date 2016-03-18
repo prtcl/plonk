f6d9ce54d2a1766ca05a806ff19dbb1f21d9111c
Cory O'Brien <cory@prtcl.cc>
2016-03-18 16:14:36 +0000

    abstract the core logic from plonk.dust and plonk.walk into a new timer plonk.variable

--- test/test.js
+++ test/test.js
@@ -93,9 +93,8 @@ describe('plonk', function () {
         time.should.be.a('number');
         i.should.be.a('number');
         stop.should.be.a('function');
-        if (i === 2) stop(i);
-        return i;
-      }).should.eventually.equal(2);
+        if (i === 2) stop();
+      }).should.eventually.be.a('number');
     });
   });
 
@@ -209,6 +208,27 @@ describe('plonk', function () {
     });
   });
 
+  describe('variable', function () {
+    it('should be a timer that returns a promise', function () {
+      plonk.should.have.property('variable');
+      plonk.variable(10, function (time, i, stop) { stop(); }).should.be.an.instanceof(Promise);
+      var start = plonk.now();
+      return plonk.variable(10, function (time, i, stop) {
+        plonk.now().should.be.above(start);
+        stop();
+      });
+    });
+    it('should pass a stop function, delay time, and iteration count into the tick callback', function () {
+      return plonk.variable(Math.random() * 100, function (time, i, stop) {
+        time.should.be.a('number');
+        i.should.be.a('number');
+        stop.should.be.a('function');
+        if (i === 2) stop();
+        return Math.random() * 100;
+      }).should.eventually.be.a('number');
+    });
+  });
+
   describe('wait', function () {
     it('should be a wrapper for setTimeout that returns a promise', function () {
       plonk.should.have.property('wait');
@@ -231,9 +251,8 @@ describe('plonk', function () {
         time.should.be.a('number');
         i.should.be.a('number');
         stop.should.be.a('function');
-        if (i === 2) stop(i);
-        return i;
-      }).should.eventually.equal(2);
+        if (i === 2) stop();
+      }).should.eventually.be.a('number');
     });
   });
 
