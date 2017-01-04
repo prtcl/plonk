52c05faa4ad42ba7a4af890e81287a38bd8a16d0
Cory O'Brien <cory@prtcl.cc>
2017-01-03 23:45:01 -0500

    refactor and test now

---
+++ test/util/now.spec.js
@@ -0,0 +1,19 @@
+
+import test from 'tape';
+
+import now, { performanceNowHandler } from '../../lib/util/now';
+
+test('util/now', (t) => {
+  t.equal(typeof now, 'function', 'now is a function');
+  t.equal(typeof now(), 'number', `${now()} is a number`);
+
+  t.equal(typeof performanceNowHandler, 'function', 'performanceNowHandler is a function');
+  t.equal(typeof performanceNowHandler(), 'number', `${performanceNowHandler()} is a number`);
+
+  var start = now();
+  setTimeout(() => {
+    t.ok(now() > start, `${now()} is greater than ${start}`);
+    t.end();
+  }, 0);
+
+});
