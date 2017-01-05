b45572fa99faff3050819e293c0e88283abd7c58
Cory O'Brien <cory@prtcl.cc>
2017-01-05 01:47:51 -0500

    user Timer instead of setTimeout in wait

--- test/time/wait.spec.js
+++ test/time/wait.spec.js
@@ -8,9 +8,19 @@ test('time/wait', (t) => {
   t.equal(typeof wait, 'function', 'wait is a function');
   t.ok(wait() instanceof Promise, 'wait() returns a Promise');
 
-  wait(10)
+  wait()
     .then((elapsed) => {
-      t.ok(elapsed >= 9, `${elapsed} is greater than 9`);
+      t.ok(elapsed >= 0 && elapsed <= 10, `then: ${elapsed} is in 0...10`);
+
+      return wait(10);
+    })
+    .then((elapsed) => {
+      t.ok(elapsed >= 10 && elapsed <= 20, `then: ${elapsed} is in 10...20`);
+
+      return wait(100);
+    })
+    .then((elapsed) => {
+      t.ok(elapsed >= 100 && elapsed <= 110, `then: ${elapsed} is in 100...110`);
 
       t.end();
     });
