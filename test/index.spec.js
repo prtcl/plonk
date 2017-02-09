7b55f08f1d77989840492d93c18ee07e6c563d3a
Cory O'Brien <cory@prtcl.cc>
2017-02-08 22:04:34 -0500

    update main export

---
+++ test/index.spec.js
@@ -0,0 +1,32 @@
+
+import test from 'tape';
+
+import * as plonk from '../lib';
+
+test('plonk', (t) => {
+
+  const METHODS = [
+    'clamp',
+    'defer',
+    'delay',
+    'drunk',
+    'dust',
+    'env',
+    'exp',
+    'frames',
+    'metro',
+    'ms',
+    'now',
+    'rand',
+    'scale',
+    'sine',
+    'wait',
+    'walk'
+  ];
+
+  METHODS.forEach((name) => {
+    t.equal(typeof plonk[name], 'function', `plonk.${name} is a function`);
+  });
+
+  t.end();
+});
