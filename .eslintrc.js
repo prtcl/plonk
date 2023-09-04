e719e0a767721adcd40c92071e5924840219c630
Cory O'Brien <cory@prtcl.cc>
2023-09-04 18:40:14 -0400

    make sure prettier is installed and hooked up to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,11 +8,33 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended-type-checked",
+    "prettier",
   ],
   plugins: ["@typescript-eslint"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.eslint.json",
     tsconfigRootDir: __dirname,
+  },
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { varsIgnorePattern: "^_*" },
+    ],
+    "@typescript-eslint/no-var-requires": "error",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      { checksVoidReturn: false },
+    ],
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        disallowTypeAnnotations: true,
+        fixStyle: "separate-type-imports",
+      },
+    ],
+    "no-console": ["error", { allow: ["info", "error"] }],
   },
 };
