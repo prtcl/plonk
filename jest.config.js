f8079bdef3f21a0aaecc4f736cc65629fff2d7d3
Cory O'Brien <cory@prtcl.cc>
2023-09-09 13:47:58 -0400

    run tests in each package independently

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(test).ts?(x)"],
-  transform: {
-    "^.+\\.(js|ts)$": "ts-jest",
-  },
-};
