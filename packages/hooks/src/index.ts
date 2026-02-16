f83f87dcadc10be72be23f4e0d4e9de2d46ca0a8
Cory O'Brien <cory@prtcl.cc>
2026-02-16 18:42:34 -0500

    add memoized wrappers for the reminaing generators

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -2,6 +2,13 @@
 
 import 'client-only';
 
+export { useDrunk } from './useDrunk';
+export { useEnv } from './useEnv';
+export { useFold } from './useFold';
 export { useFrames, type UseFramesOptions } from './useFrames';
 export { useMetro, type UseMetroOptions } from './useMetro';
 export { usePrevious } from './usePrevious';
+export { useRand } from './useRand';
+export { useScale } from './useScale';
+export { useSine } from './useSine';
+export { useWrap } from './useWrap';
